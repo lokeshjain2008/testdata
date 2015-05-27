90c39f9a9119c9a3e70e0131f666837994091534
lokeshjain2008 <Lokesh@IND-MAC-010.local>
2015-05-27 22:29:43 +0530

    phase 0

--- app/scripts/angularfire/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('firebase.config', [])
-  .constant('FBURL', 'https://n.firebaseio.com')
-  .constant('SIMPLE_LOGIN_PROVIDERS', ['password','facebook','google','twitter'])
-
-  .constant('loginRedirectPath', '/login');
